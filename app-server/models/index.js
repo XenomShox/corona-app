04498f0546411b760bd84cec037af7911f52717f
Abdelhak Ihadjadene <xenomshox69@gmail.com>
2020-03-19 23:24:15 +0100

    second commit

--- app-server/models/index.js
+++ app-server/models/index.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost:27017/covid19", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true
-});
+mongoose.connect(
+    "mongodb+srv://xenomshox:0561182132salma@cluster0-isrir.mongodb.net/test?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        keepAlive: true
+    }
+);
 
 module.exports.User = require("./user");
 module.exports.Meeting = require("./meeting");
